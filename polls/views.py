00b8496d1343f3fbd6db8bd0e1d95e40b82d05b6
Omar Alejandro Hernández Bucio <90805470+Darkrowjkr@users.noreply.github.com>
2023-01-16 18:02:11 -0600

    fin primer dia

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hola mundo. Tu estas en el index de polls")
+
+def detail(request, question_id):
+    return HttpResponse("Estas mirando una respuesta %s." % question_id)
+
+def results(request, question_id):
+    response = "Estas mirando los resultados de la pregunta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Estas votando en la pregunta %s." % question_id)
